9a1eedf667360160697b5bdcba3edcbcd22ea82b
whi10 <r500scarlet495f@ezweb.ne.jp>
2019-07-11 01:59:21 +0900

    win or lose

--- dice.c
+++ dice.c
@@ -3,13 +3,18 @@
 #include <time.h>
  
 int main(void){
+  char name[100];
   int n1,n2;
+  printf("What is your name?\n> ");
+  scanf("%s", name);
+  printf("Hello, %s!\n",name);
   srand(time(NULL));
   n1 = rand() % 6 + 1;
   n2 = rand() % 6 + 1;
   printf("Rolling the dice...\n");
   printf("Dice1: %d\n",n1);
   printf("Dice2: %d\n",n2);
-  printf("Total value: %d\n",n1+n2);
+  if(n1+n2>7) printf("you won");
+  else printf("you lost");
   return 0;
 }
